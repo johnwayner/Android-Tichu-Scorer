e8060fd2b997d28700e089cdf94ec8edd9f3d60c
johnwayner <johnwayner@johnwayner-xub>
2010-05-24 22:37:04 -0500

    Added Game Creation activity/layout.  Currently can only select players for specific seats and add players.  Can also get to the manage players activity.  Can't actually create a game, yet.

--- src/com/johnwayner/android/tichu/scorer/actions/CreateNewGame.java
+++ src/com/johnwayner/android/tichu/scorer/actions/CreateNewGame.java
@@ -1,11 +1,21 @@
 package com.johnwayner.android.tichu.scorer.actions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.johnwayner.android.tichu.scorer.R;
+import com.johnwayner.android.tichu.scorer.dialogs.ManagePlayerDialogBuilder;
+import com.johnwayner.android.tichu.scorer.dialogs.ManagePlayerDialogBuilder.ManagePlayerDialogCallBack;
 import com.johnwayner.android.tichu.scorer.models.ApplicationState;
 import com.johnwayner.android.tichu.scorer.models.Player;
 
@@ -14,10 +24,97 @@ public class CreateNewGame extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ApplicationState appState = ApplicationState.getState(this.getApplicationContext());
-        appState.getPlayers().add(new Player("Steve"));
-        setContentView(R.layout.main);
+        setContentView(R.layout.create_game);
+        setupSpinners();
+                
     }   
+    
+    protected void setupSpinners() {
+		setupSpinner(R.id.north_spinner, R.string.north_abbrv);
+		setupSpinner(R.id.east_spinner, R.string.east_abbrv);
+		setupSpinner(R.id.south_spinner, R.string.south_abbrv);
+		setupSpinner(R.id.west_spinner, R.string.west_abbrv);
+		setupSpinner(R.id.southeast_spinner, R.string.southeast_abbrv);
+    }
+    
+    protected List<Player> getChoiceList(int posRsrcId) {
+    	ApplicationState appState = ApplicationState.getState(this.getApplicationContext());
+    	Player chooseOnePlayer = new Player(getString(posRsrcId));
+        ArrayList<Player> choiceList = new ArrayList<Player>(appState.getPlayers());
+        choiceList.add(0, chooseOnePlayer);
+        return choiceList;
+    }
+    
+    protected void setupSpinner(int spinnerRsrcId, int posRsrcId) {
+    	Spinner spinner = (Spinner)findViewById(spinnerRsrcId);
+        
+        ArrayAdapter<Player> playerAdapter = 
+        	new ArrayAdapter<Player>(
+        				this, 
+        				android.R.layout.simple_spinner_item,
+        				getChoiceList(posRsrcId));
+        playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        
+        spinner.setAdapter(playerAdapter);
+        spinner.postInvalidate();
+        playerArrayAdapters.add(playerAdapter);
+    }
+    	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.create_game_options_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.create_game_new_player_menu_item:
+			showDialog(ManagePlayerDialogBuilder.DIALOG_NEW);
+			return true;
+		case R.id.create_game_clear_seats_menu_item:
+			setupSpinners();
+			return true;
+		case R.id.create_game_manage_players_menu_item:
+			startActivity(new Intent("com.johnwayner.android.tichu.scorer.MANAGE_PLAYERS"));
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+    protected Dialog onCreateDialog(int id)
+    {
+        return ManagePlayerDialogBuilder.createEditorDialog(this, id, new ManagePlayerDialogCallBack() {
+			
+			@Override
+			public void editPlayer(String name) {
+				//Shouldn't be calling this.
+			}
+			
+			@Override
+			public void addNewPlayer(String name) {
+				Player newPlayer = new Player(name);
+				ApplicationState.getState(CreateNewGame.this).getPlayers().add(newPlayer);
+				for(ArrayAdapter<Player> adapter : playerArrayAdapters) {
+					adapter.add(newPlayer);
+				}
+			}
+		});
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+    	super.onPrepareDialog(id, dialog);
+    	
+    	ManagePlayerDialogBuilder.onPrepareDialog(id, dialog, null);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	setupSpinners();
+    }
     
     @Override
     protected void onPause() {
@@ -29,4 +126,6 @@ public class CreateNewGame extends Activity {
 		}
     	super.onPause();
     }
+    
+    protected List<ArrayAdapter<Player>> playerArrayAdapters = new ArrayList<ArrayAdapter<Player>>();
 }
--- src/com/johnwayner/android/tichu/scorer/actions/ManagePlayers.java
+++ src/com/johnwayner/android/tichu/scorer/actions/ManagePlayers.java
@@ -5,24 +5,19 @@ package com.johnwayner.android.tichu.scorer.actions;
 
 import java.io.IOException;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Selection;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.AdapterView.OnItemClickListener;
 
 import com.johnwayner.android.tichu.scorer.R;
+import com.johnwayner.android.tichu.scorer.dialogs.ManagePlayerDialogBuilder;
+import com.johnwayner.android.tichu.scorer.dialogs.ManagePlayerDialogBuilder.ManagePlayerDialogCallBack;
 import com.johnwayner.android.tichu.scorer.models.ApplicationState;
 import com.johnwayner.android.tichu.scorer.models.Player;
 
@@ -31,8 +26,7 @@ import com.johnwayner.android.tichu.scorer.models.Player;
  *
  */
 public class ManagePlayers extends ListActivity {
-	private final static int DIALOG_NEW = 0;
-	private final static int DIALOG_EDIT = 1;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +49,7 @@ public class ManagePlayers extends ListActivity {
 			public void onItemClick(AdapterView<?> parent, View view, int position,
 					long arg3) {
 				playerToEdit = arrayAdapter.getItem(position);
-				showDialog(DIALOG_EDIT);
+				showDialog(ManagePlayerDialogBuilder.DIALOG_EDIT);
 				
 			}
 		    
@@ -75,7 +69,7 @@ public class ManagePlayers extends ListActivity {
 		case R.id.new_player_menu_item:
 //			startActivity(new Intent(EditPlayer.NEW_USER_ACTION_NAME));
 //			arrayAdapter.notifyDataSetChanged();
-			showDialog(DIALOG_NEW);
+			showDialog(ManagePlayerDialogBuilder.DIALOG_NEW);
 			return true;
 		case R.id.delete_all_menu_item:
 			arrayAdapter.clear();
@@ -87,87 +81,27 @@ public class ManagePlayers extends ListActivity {
 	@Override
     protected Dialog onCreateDialog(int id)
     {
-        return createEditorDialog(id);
-    }
-
-    private Dialog createEditorDialog(final int id)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        
-        if(DIALOG_NEW == id) {
-        	builder.setTitle(R.string.create_new_player_title);
-        } else {
-        	builder.setTitle(R.string.edit_player_title);
-        }
-
-        View content = getLayoutInflater().inflate(R.layout.player_edit,
-            (ViewGroup) findViewById(R.id.player_edit_layout));
-        builder.setView(content);
-        
-        final EditText nameField = (EditText) 
-			content.findViewById(R.id.name_edit_text);
-        
-        nameField.setOnKeyListener(new OnKeyListener() {
+        return ManagePlayerDialogBuilder.createEditorDialog(this, id, new ManagePlayerDialogCallBack() {
 			
 			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if((null != event) && (KeyEvent.KEYCODE_ENTER == event.getKeyCode())) {
-					return true;
-				}
-				return false;
+			public void editPlayer(String name) {
+				playerToEdit.setName(name);
+				arrayAdapter.notifyDataSetChanged();
+			}
+			
+			@Override
+			public void addNewPlayer(String name) {
+				arrayAdapter.add(
+                		new Player(name));
 			}
 		});
-        
-        builder.setPositiveButton(R.string.player_edit_save_button_label,
-            new DialogInterface.OnClickListener()
-            {
-
-                public void onClick(DialogInterface dialog, int which)
-                {
-                    String newName = nameField.getText().toString();
-
-                    if(DIALOG_NEW == id) {
-                    	arrayAdapter.add(
-                    		new Player(newName));
-                    } else {
-                    	playerToEdit.setName(newName);
-                    }
-                    
-                    arrayAdapter.notifyDataSetChanged();                    
-                    dialog.dismiss();
-                }
-            });
-
-        builder.setNegativeButton(R.string.cancel_button_label,
-            new DialogInterface.OnClickListener()
-            {
-
-                public void onClick(DialogInterface dialog, int which)
-                {
-                    dialog.dismiss();
-                }
-            });
-
-        return builder.create();
     }
-    
+
     @Override
     protected void onPrepareDialog(int id, Dialog dialog) {
     	super.onPrepareDialog(id, dialog);
     	
-    	EditText nameField = (EditText) 
-    		dialog.findViewById(R.id.name_edit_text);
-    	
-        if(DIALOG_EDIT == id) {
-        	nameField.setText(playerToEdit.getName());
-        } else {
-        	nameField.setText("");
-        }
-        
-        //Work around to put the cursor at the end
-        Selection.setSelection(
-        		nameField.getText(),
-        		nameField.getText().length());
+    	ManagePlayerDialogBuilder.onPrepareDialog(id, dialog, playerToEdit);
     }
 	
     @Override
